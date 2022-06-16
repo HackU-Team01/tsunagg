89f4fe83f0b1ba0fd2871167f4ab4ed8e8be61d1
井手翔陽 <shouhi605@gmail.com>
2022-06-16 12:52:19 +0900

    httpsのローカル環境を用意

--- frontend/src/lib/auth/AuthProvider.tsx
+++ frontend/src/lib/auth/AuthProvider.tsx
@@ -1,10 +1,9 @@
-import firebase from "firebase/app";
-import { FC, createContext, useEffect, useState } from "react";
-import { auth } from "../firebase";
-import { useRouter } from "next/router";
+import firebase from 'firebase/app';
+import { useRouter } from 'next/router';
+import { FC, createContext, useEffect, useState } from 'react';
 
+import { auth } from '../firebase';
 
- 
 type AuthContextProps = {
   currentUser: firebase.User | null | undefined;
 };
@@ -17,38 +16,31 @@ type Props = {
 
 const AuthProvider: FC<Props> = ({ children }) => {
   const router = useRouter();
-  const [currentUser, setCurrentUser] = useState<
-    firebase.User | null | undefined
-  >(undefined);
+  const [currentUser, setCurrentUser] = useState<firebase.User | null | undefined>(undefined);
 
   useEffect(() => {
     auth.onAuthStateChanged(async (user) => {
       setCurrentUser(user);
       if (user) {
         // ログイン済みのユーザー情報があるかをチェック
-        const userDoc = await firebase
-          .firestore()
-          .doc(`user/${user.uid}`)
-          .get();
+        const userDoc = await firebase.firestore().doc(`users/${user.uid}`).get();
         if (!userDoc.exists) {
+          //slack認証のレスポンスを待つ
+
           //docがなければ作る
           userDoc.ref.set({
-            name: "",
-            avatarUrl: "",
+            name: '',
+            avatarUrl: '',
             created_at: firebase.firestore.FieldValue.serverTimestamp(),
           });
         }
       } else {
-        router.push("/signin");
+        router.push('/signin');
       }
     });
   }, []);
 
-  return (
-    <AuthContext.Provider value={{ currentUser }}>
-      {children}
-    </AuthContext.Provider>
-  );
+  return <AuthContext.Provider value={{ currentUser }}>{children}</AuthContext.Provider>;
 };
 
 export { AuthContext, AuthProvider };
