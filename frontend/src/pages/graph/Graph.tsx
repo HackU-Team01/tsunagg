4305ded92d97704ca885e488ab1976831326fc78
Kahuka0506 <kou22uhneiom@icloud.com>
2022-06-29 22:51:40 +0900

    グラフ絞りこみ追加

--- frontend/src/pages/graph/Graph.tsx
+++ frontend/src/pages/graph/Graph.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import React from 'react';
 
 import { db } from '../../lib/firebase';
+import Graph_filter_place from './Graph_filter_place';
+import Graph_filter_hobby from './Graph_filter_hobby';
 
 const uuId = 'User4KpZPzCR6zJy0KUX';
 
@@ -10,83 +12,6 @@ var nodes = null;
 var nodes_number = -1;
 var edges = null;
 var network = null;
-
-function init_graph() {
-  var DIR = '';
-  nodes = [
-    {
-      id: 1,
-      value: 150,
-      shape: 'circularImage',
-      image: DIR + 'usericon1.png',
-      label: 'User1',
-      title: 'name\n hobby\n place',
-    },
-    {
-      id: 2,
-      value: 20,
-      shape: 'circularImage',
-      image: DIR + 'usericon1.png',
-      label: 'User2',
-    },
-    {
-      id: 3,
-      value: 20,
-      shape: 'circularImage',
-      image: DIR + 'usericon1.png',
-      label: 'User3',
-    },
-    {
-      id: 4,
-      value: 20,
-      shape: 'circularImage',
-      image: DIR + 'usericon1.png',
-      label: 'User4',
-    },
-    {
-      id: 5,
-      value: 20,
-      shape: 'circularImage',
-      image: DIR + 'usericon1.png',
-      label: 'User5',
-    },
-    {
-      id: 6,
-      value: 20,
-      shape: 'circularImage',
-      image: DIR + 'usericon1.png',
-      label: 'User6',
-    },
-    {
-      id: 7,
-      value: 20,
-      shape: 'circularImage',
-      image: DIR + 'usericon1.png',
-      label: 'User7',
-      //shape: "star",
-    },
-  ];
-
-  edges = [
-    { from: 1, to: 2, value: 2, title: 'edge User1 User2\n hobby1, hobby2' },
-    { from: 1, to: 3, value: 1 },
-    { from: 1, to: 4, value: 1 },
-    { from: 1, to: 5, value: 3 },
-    { from: 3, to: 6, value: 1 },
-    { from: 1, to: 7, value: 1 },
-  ];
-}
-
-function addNode() {
-  nodes_number++;
-  nodes.push({
-    id: nodes_number,
-    value: 20,
-    shape: 'circularImage',
-    image: DIR + 'usericon1.png',
-    label: 'User' + nodes_number,
-  });
-}
 
 function draw() {
   var container = document.getElementById('mynetwork');
@@ -137,14 +62,14 @@ function draw() {
         customScalingFunction: function (min, max, total, value) {
           return value / total;
         },
-        min: 20,
-        max: 30,
+        min: 10,
+        max: 70,
       },
       title: undefined,
     },
     edges: {
       color: 'orangered',
-      length: 160,
+      length: 120,
       font: {
         color: '#343434',
         size: 31,
@@ -156,21 +81,27 @@ function draw() {
 }
 
 export default function DrawNetwork() {
-  const handleOnClick_testtest = async () => {
-    const head = document.getElementsByTagName('head')[0] as HTMLElement;
-    const scriptUrl = document.createElement('script');
-    scriptUrl.type = 'text/javascript';
-    scriptUrl.src = 'https://unpkg.com/vis-network/standalone/umd/vis-network.min.js';
-    head.appendChild(scriptUrl);
-
-    if (nodes_number == -1) {
-      nodes_number = 7;
-      init_graph();
-      return;
+  var fliter_flug = 0;
+  const handleOnClick_filer_on_off = async () => {
+    const elem = document.getElementById('filter_html');
+    if (fliter_flug == 1) {
+      elem.className = 'hidden';
+      fliter_flug = 0;
+    } else {
+      elem.className = '';
+      fliter_flug = 1;
     }
-    addNode();
-    draw();
-    console.log(1);
+    //elem.className="z-[20] m-2 invisible";
+  };
+  const handleOnClick_filter_clear = async () => {
+    document.getElementById('edge_w').value = '0';
+    document.getElementById('select_Place_born').value = '-1';
+    document.getElementById('select_Place_live').value = '-1';
+    document.getElementById('select_hobby1').value = '-1';
+    document.getElementById('select_hobby2').value = '-1';
+    document.getElementById('select_hobby3').value = '-1';
+    document.getElementById('select_hobby4').value = '-1';
+    document.getElementById('select_friend_type').value = '-1';
   };
 
   const handleOnClick_draw_network = async () => {
@@ -195,7 +126,7 @@ export default function DrawNetwork() {
           nodes = [
             {
               id: 0,
-              value: 30,
+              value: 80,
               shape: 'circularImage',
               image: DIR + 'usericon1.png',
               label: uuId,
@@ -220,9 +151,7 @@ export default function DrawNetwork() {
       } catch (err) {
         console.log(`Error!: ${JSON.stringify(err)}`);
       }
-    })();
 
-    (async () => {
       try {
         const userRef = db.collection('match_user_sample').doc(uuId);
         db.runTransaction((transaction) => {
@@ -260,8 +189,6 @@ export default function DrawNetwork() {
         })
           .then(function () {
             console.log(1111);
-
-            console.log(nodes);
             console.log(edges);
             draw();
           })
@@ -299,10 +226,12 @@ export default function DrawNetwork() {
         user_node_data.forEach((userDoc) => {
           //console.log(postDoc.id, ' => ', JSON.stringify(postDoc.data()))
           //console.log(userDoc.data());
+          const node_sz = 20;
+          if (userDoc.id == uuId) node_sz = 60;
           map_user_node.set(userDoc.id, cnt);
           nodes.push({
             id: cnt,
-            value: 30,
+            value: node_sz,
             shape: 'circularImage',
             image: DIR + 'usericon1.png',
             label: userDoc.id,
@@ -359,7 +288,178 @@ export default function DrawNetwork() {
                     edges.push({
                       from: map_user_node.get(name),
                       to: map_user_node.get(e),
-                      value: tokensMap[e].length,
+                      value: tokensMap[e].length * 0.5,
+                      title: edge_title,
+                    });
+                  }
+                });
+              });
+            })
+              .then(function () {
+                if (ff == cnt - 1) {
+                  console.log('finish');
+                  draw();
+                }
+              })
+              .catch((error) => {
+                console.log('Transaction failed: ', error);
+              });
+          } catch (err) {
+            console.log(`Error!: ${JSON.stringify(err)}`);
+          }
+        }
+        for (var i = 0; i < cnt; i++) {
+          //console.log(i)
+          await create_edge(nodes[i].label, i);
+        }
+      } catch (err) {
+        console.log(`Error: ${JSON.stringify(err)}`);
+      }
+    })();
+  };
+
+  const handleOnClick_draw_network_filter = async () => {
+    const filter_edge_w = document.getElementById('edge_w').value;
+    const fliter_place_born = document.getElementById('select_Place_born').value;
+    const fliter_place_live = document.getElementById('select_Place_live').value;
+    var filter_hobby = [];
+    filter_hobby.push(document.getElementById('select_hobby1').value);
+    filter_hobby.push(document.getElementById('select_hobby2').value);
+    filter_hobby.push(document.getElementById('select_hobby3').value);
+    filter_hobby.push(document.getElementById('select_hobby4').value);
+    const filter_friend_type = document.getElementById('select_friend_type').value;
+    console.log(filter_edge_w, fliter_place_born, fliter_place_live);
+    console.log(filter_hobby);
+    console.log(filter_friend_type);
+
+    const head = document.getElementsByTagName('head')[0] as HTMLElement;
+    const scriptUrl = document.createElement('script');
+    scriptUrl.type = 'text/javascript';
+    scriptUrl.src = 'https://unpkg.com/vis-network/standalone/umd/vis-network.min.js';
+    head.appendChild(scriptUrl);
+
+    nodes = [];
+    edges = [];
+    var cnt = 0;
+
+    var user_node_data = null;
+    var map_user_node = new Map<string, number>();
+    var edge_all: number[][] = new Array();
+    var edge_all_data: string[][] = new Array();
+
+    var used_user = new Set();
+
+    (async () => {
+      try {
+        user_node_data = await db.collection('user_info_sample').get();
+        console.log('data size: ' + user_node_data.size);
+
+        user_node_data.forEach((userDoc) => {
+          var use = 0;
+          var filter_on = 0;
+          if (fliter_place_born != '-1') {
+            filter_on = 1;
+            if (
+              userDoc.get('Attribute').Place_born[1] == fliter_place_born ||
+              userDoc.get('Attribute').Place_born[0] == fliter_place_born
+            )
+              use = 1;
+          }
+          if (fliter_place_live != -1) {
+            filter_on = 1;
+            if (
+              userDoc.get('Attribute').Place_Live[1] == fliter_place_live ||
+              userDoc.get('Attribute').Place_Live[0] == fliter_place_live
+            )
+              use = 1;
+          }
+          userDoc.get('Attribute').Hobby.forEach((x) => {
+            filter_hobby.forEach((y) => {
+              if (y != '-1') {
+                filter_on = 1;
+                if (x == y) use = 1;
+              }
+            });
+          });
+          if (filter_friend_type != '-1') {
+            filter_on = 1;
+            userDoc.get('Attribute').Hobby.forEach((x) => {
+              if (x == filter_friend_type) {
+                use = 1;
+              }
+            });
+          }
+          if (filter_on == 0 || use == 1) {
+            var node_sz = 20;
+            if (userDoc.id == uuId) node_sz = 60;
+            map_user_node.set(userDoc.id, cnt);
+            used_user.add(userDoc.id);
+            nodes.push({
+              id: cnt,
+              value: node_sz,
+              shape: 'circularImage',
+              image: DIR + 'usericon1.png',
+              label: userDoc.id,
+              physics: false,
+              title:
+                'uuid: ' +
+                userDoc.id +
+                '\n' +
+                '出身: ' +
+                userDoc.get('Attribute').Place_born[1] +
+                '\n' +
+                '居住地: ' +
+                userDoc.get('Attribute').Place_Live[1] +
+                '\n' +
+                '趣味: ' +
+                userDoc.get('Attribute').Hobby +
+                '\n' +
+                '一言: ' +
+                userDoc.get('Sentence') +
+                '\n',
+            });
+            cnt++;
+          }
+        });
+        console.log(used_user);
+
+        for (var i = 0; i < cnt; i++) {
+          edge_all[i] = new Array();
+          edge_all_data[i] = new Array();
+          for (var j = 0; j < cnt; j++) {
+            edge_all[i][j] = 0;
+            edge_all_data[i][j] = '';
+          }
+        }
+        async function create_edge(name: string, ff, number) {
+          //console.log(name)
+          try {
+            const userRef = db.collection('match_user_sample').doc(name);
+            db.runTransaction((transaction) => {
+              return transaction.get(userRef).then((tokenSettingsDocSnapshot) => {
+                if (!tokenSettingsDocSnapshot.exists) {
+                  throw 'Document does not exist!';
+                }
+
+                let tokensMap = tokenSettingsDocSnapshot.data().match_user_info;
+
+                //console.log(tokensMap);
+                Object.keys(tokensMap).forEach((e) => {
+                  //console.log(e, tokensMap[e]);
+                  if (
+                    used_user.has(e) &&
+                    map_user_node.get(name) < map_user_node.get(e) &&
+                    tokensMap[e].length >= filter_edge_w
+                  ) {
+                    var edge_title =
+                      name + '-' + e + '\n 繋がりの強さ: ' + tokensMap[e].length + '\n 共通点:  \n';
+                    tokensMap[e].forEach((x) => {
+                      edge_title += '　　　　　' + x + '\n';
+                    });
+                    edges.push({
+                      from: map_user_node.get(name),
+                      to: map_user_node.get(e),
+                      value: tokensMap[e].length * 0.5,
                       title: edge_title,
                     });
                   }
@@ -391,12 +491,11 @@ export default function DrawNetwork() {
 
   return (
     <div className="w-full">
-      <div className="absolute z-[20]">
+      <div className="absolute z-[20] w-full">
         <button
           type="button"
           className="z-[10] inline-block py-2.5 px-6 text-xs font-medium leading-tight text-gray-900 bg-gray-100 thover:bg-gray-300 focus:bg-gray-300 active:bg-gray-400 rounded-full border-2 focus:outline-none focus:ring-0 shadow-md hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out border-gray-10 hover:scale-110 m-5"
           onClick={() => {
-            //handleOnClick_testtest();
             handleOnClick_draw_network();
           }}
         >
@@ -407,12 +506,127 @@ export default function DrawNetwork() {
           type="button"
           className="z-[10] inline-block py-2.5 px-6 text-xs font-medium leading-tight text-gray-900 bg-gray-100 thover:bg-gray-300 focus:bg-gray-300 active:bg-gray-400 rounded-full border-2 focus:outline-none focus:ring-0 shadow-md hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out border-gray-10 hover:scale-110 m-5"
           onClick={() => {
-            //handleOnClick_testtest();
             handleOnClick_draw_network_all();
           }}
         >
           グラフ描画 all
         </button>
+
+        <button
+          type="button"
+          className="z-[10] inline-block py-2.5 px-6 text-xs font-medium leading-tight text-gray-900 bg-gray-100 thover:bg-gray-300 focus:bg-gray-300 active:bg-gray-400 rounded-full border-2 focus:outline-none focus:ring-0 shadow-md hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out border-gray-10 hover:scale-110 m-5"
+          onClick={() => {
+            handleOnClick_filer_on_off();
+          }}
+        >
+          絞りこみ条件　
+          <svg className="flex w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+          </svg>
+        </button>
+
+        <div className="hidden" id="filter_html">
+          <div className="z-[20] m-2 space-y-1 space-x-2 visible rounded-3xl border-2 border-gray-100 shadow-md w-1/2 bg-gradient-to-tr from-gray-50 to-gray-100 bg-opacity-0">
+            <div className="m-3">
+              <span>出身地 </span>
+              <select
+                id="select_Place_born"
+                className="py-2 px-2 text-sm text-gray-900 bg-white focus:bg-white border border-gray-300 appearance-none bg-whilte"
+              >
+                <option value="-1">出身地</option>
+                <Graph_filter_place />
+              </select>
+              <br />
+
+              <span>居住地 </span>
+              <select
+                id="select_Place_live"
+                className="py-2 px-2 text-sm text-gray-900 bg-white focus:bg-white border border-gray-300 appearance-none bg-whilte"
+              >
+                <option value="-1">居住地</option>
+                <Graph_filter_place />
+              </select>
+              <br />
+
+              <span>趣味 </span>
+              <br />
+              <select
+                id="select_hobby1"
+                className="py-2 px-2 text-sm text-gray-900 bg-white focus:bg-white border border-gray-300 appearance-none bg-whilte"
+              >
+                <option value="-1">趣味1</option>
+                <Graph_filter_hobby />
+              </select>
+              <select
+                id="select_hobby2"
+                className="py-2 px-2 text-sm text-gray-900 bg-white focus:bg-white border border-gray-300 appearance-none bg-whilte"
+              >
+                <option value="-1">趣味2</option>
+                <Graph_filter_hobby />
+              </select>
+              <select
+                id="select_hobby3"
+                className="py-2 px-2 text-sm text-gray-900 bg-white focus:bg-white border border-gray-300 appearance-none bg-whilte"
+              >
+                <option value="-1">趣味3</option>
+                <Graph_filter_hobby />
+              </select>
+              <select
+                id="select_hobby4"
+                className="py-2 px-2 text-sm text-gray-900 bg-white focus:bg-white border border-gray-300 appearance-none bg-whilte"
+              >
+                <option value="-1">趣味4</option>
+                <Graph_filter_hobby />
+              </select>
+              <br />
+
+              <span>探したい相手 </span>
+              <select
+                id="select_friend_type"
+                className="py-2 px-2 text-sm text-gray-900 bg-white focus:bg-white border border-gray-300 appearance-none bg-whilte"
+              >
+                <option value="-1">探したい相手</option>
+                <option value="友達募集">友達募集</option>
+                <option value="遊び相手">遊び相手</option>
+                <option value="話し相手">話し相手</option>
+                <option value="趣味仲間">趣味仲間</option>
+                <option value="仕事仲間">仕事仲間</option>
+                <option value="勉強仲間">勉強仲間</option>
+              </select>
+              <br />
+
+              <span>繋がりの強さ </span>
+              <input
+                type="number"
+                id="edge_w"
+                min="0"
+                defaultValue="0"
+                className="py-2 px-2 text-sm text-gray-900 bg-white focus:bg-white border border-gray-300 bg-whilte"
+              />
+              <br />
+
+              <button
+                type="button"
+                className="z-[101] inline-block py-2.5 px-6 text-xs font-medium leading-tight text-gray-900 bg-gray-100 thover:bg-gray-300 focus:bg-gray-300 active:bg-gray-400 rounded-full border-2 focus:outline-none focus:ring-0 shadow-md hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out border-gray-10 hover:scale-110 m-3"
+                onClick={() => {
+                  handleOnClick_filter_clear();
+                }}
+              >
+                条件クリア
+              </button>
+
+              <button
+                type="button"
+                className="z-[101] fnline-block py-2.5 px-6 text-xs font-medium leading-tight text-gray-900 bg-gray-100 thover:bg-gray-300 focus:bg-gray-300 active:bg-gray-400 rounded-full border-2 focus:outline-none focus:ring-0 shadow-md hover:shadow-lg focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out border-gray-10 hover:scale-110 m-3"
+                onClick={() => {
+                  handleOnClick_draw_network_filter();
+                }}
+              >
+                グラフ描画
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="w-full h-screen bg-white .place-content-center" id="mynetwork"></div>
---
+++ frontend/src/pages/graph/Graph_filter_hobby.tsx
@@ -0,0 +1,111 @@
+export default function create_hobby_pulldown() {
+  //let aa = "";
+  //for (const [key, value] of map_place) aa += "<option value=\"" + key + "\">" + key + "</option>";
+  //console.log(aa);
+
+  //console.log(map_place);
+
+  return (
+    <>
+      <option value="スポーツ">スポーツ</option>
+      <option value="野球">野球</option>
+      <option value="サッカー">サッカー</option>
+      <option value="卓球">卓球</option>
+      <option value="バレーボール">バレーボール</option>
+      <option value="バドミントン">バドミントン</option>
+      <option value="陸上">陸上</option>
+      <option value="バスケットボール">バスケットボール</option>
+      <option value="テニス">テニス</option>
+      <option value="ラグビー">ラグビー</option>
+      <option value="水泳">水泳</option>
+      <option value="その他">その他</option>
+      <option value="ライフスタイル">ライフスタイル</option>
+      <option value="サウナ">サウナ</option>
+      <option value="睡眠">睡眠</option>
+      <option value="ドライブ">ドライブ</option>
+      <option value="カフェ巡り">カフェ巡り</option>
+      <option value="美術館">美術館</option>
+      <option value="映画">映画</option>
+      <option value="カラオケ">カラオケ</option>
+      <option value="ダンス">ダンス</option>
+      <option value="舞台">舞台</option>
+      <option value="Netflix">Netflix</option>
+      <option value="飲み">飲み</option>
+      <option value="旅行">旅行</option>
+      <option value="ゲーム">ゲーム</option>
+      <option value="モンハン">モンハン</option>
+      <option value="スプラトゥーン">スプラトゥーン</option>
+      <option value="ポケモン">ポケモン</option>
+      <option value="マリオ">マリオ</option>
+      <option value="フォートナイト">フォートナイト</option>
+      <option value="Apex">Apex</option>
+      <option value="スマブラ">スマブラ</option>
+      <option value="ドラクエ">ドラクエ</option>
+      <option value="FF">FF</option>
+      <option value="キングダムハーツ">キングダムハーツ</option>
+      <option value="ELDENRING">ELDENRING</option>
+      <option value="龍が如く">龍が如く</option>
+      <option value="アニメ">アニメ</option>
+      <option value="コードギアス">コードギアス</option>
+      <option value="STEINS;GATE">STEINS;GATE</option>
+      <option value="進撃の巨人">進撃の巨人</option>
+      <option value="ヴァイオレットエヴァーガーデン">ヴァイオレットエヴァーガーデン</option>
+      <option value="銀魂">銀魂</option>
+      <option value="ハイキュー">ハイキュー</option>
+      <option value="魔法少女まどか★マギカ">魔法少女まどか★マギカ</option>
+      <option value="宇宙よりも遠い場所">宇宙よりも遠い場所</option>
+      <option value="僕のヒーローアカデミア">僕のヒーローアカデミア</option>
+      <option value="エヴァンゲリオン">エヴァンゲリオン</option>
+      <option value="音楽">音楽</option>
+      <option value="JPOP">JPOP</option>
+      <option value="SEKAINOOWARI">SEKAINOOWARI</option>
+      <option value="Official髭男dism">Official髭男dism</option>
+      <option value="TaniYuuki">TaniYuuki</option>
+      <option value="マカロニえんぴつ">マカロニえんぴつ</option>
+      <option value="Ado">Ado</option>
+      <option value="Uru">Uru</option>
+      <option value="Aimer">Aimer</option>
+      <option value="backnumber">backnumber</option>
+      <option value="米津玄師">米津玄師</option>
+      <option value="YOASOBI">YOASOBI</option>
+      <option value="アニメ">アニメ</option>
+      <option value="Lisa">Lisa</option>
+      <option value="QUEENDOM">QUEENDOM</option>
+      <option value="Chinozo">Chinozo</option>
+      <option value="DECO*27">DECO*27</option>
+      <option value="fripSide">fripSide</option>
+      <option value="supercell">supercell</option>
+      <option value="K-pop">K-pop</option>
+      <option value="BTS">BTS</option>
+      <option value="IVE">IVE</option>
+      <option value="LE">LE</option>
+      <option value="SSERAFIM">SSERAFIM</option>
+      <option value="TWICE">TWICE</option>
+      <option value="PSY">PSY</option>
+      <option value="HipHop">HipHop</option>
+      <option value="CreepyNuts">CreepyNuts</option>
+      <option value="ちゃんみな">ちゃんみな</option>
+      <option value="Awich">Awich</option>
+      <option value="WILYWNKA">WILYWNKA</option>
+      <option value="VaVa">VaVa</option>
+      <option value="Tohji">Tohji</option>
+      <option value="¥ellow">¥ellow</option>
+      <option value="Bucks">Bucks</option>
+      <option value="ROCK">ROCK</option>
+      <option value="SaucyDog">SaucyDog</option>
+      <option value="優理">優理</option>
+      <option value="Novelbright">Novelbright</option>
+      <option value="Mrs.GREENAPPLE">Mrs.GREENAPPLE</option>
+      <option value="VAUNDY">VAUNDY</option>
+      <option value="ONEOKROCK">ONEOKROCK</option>
+      <option value="UVERWORLD">UVERWORLD</option>
+      <option value="MyHairisBad">MyHairisBad</option>
+      <option value="SNS">SNS</option>
+      <option value="Facebook">Facebook</option>
+      <option value="Twitter">Twitter</option>
+      <option value="LINE">LINE</option>
+      <option value="Instagram">Instagram</option>
+      <option value="友達募集">友達募集</option>
+    </>
+  );
+}
