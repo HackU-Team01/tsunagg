2ff6aa268da2e46d17937ec558c8c62ac916b77d
aqucial2957 <70082504+aqucial2957@users.noreply.github.com>
2022-06-30 22:02:59 +0900

    ディレクトリ構成を変更

---
+++ backend/model/chat/chat.go
@@ -0,0 +1,41 @@
+package chat
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+
+	"cloud.google.com/go/firestore"
+	"github.com/slack-go/slack"
+)
+
+const botTokenVar = "SLACK_BOT_TOKEN"
+
+func NewSlackBotClient(options ...slack.Option) (*slack.Client, error) {
+	token := os.Getenv(botTokenVar)
+	if token == "" {
+		errMsg := fmt.Sprintf("environmental variable `%s` is not set", botTokenVar)
+		return nil, errors.New(errMsg)
+	}
+	return slack.New(token, options...), nil
+}
+
+func NewSlackUserClient(ctx context.Context, c *firestore.Client, uuid string, options ...slack.Option) (*slack.Client, error) {
+	dsnap, err := c.Collection("users").Doc(uuid).Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	token, err := dsnap.DataAt("OAuthToken")
+	if err != nil {
+		return nil, err
+	}
+
+	tokenStr, ok := token.(string)
+	if !ok {
+		return nil, errors.New("`OAuthToken` cannot be casted to string")
+	}
+
+	return slack.New(tokenStr, options...), nil
+}
